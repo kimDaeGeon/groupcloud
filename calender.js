8b1bee5b8a9b460c3105e24fc706b66ddd81bc3d
toothless <52105661+kimDaeGeon@users.noreply.github.com>
2020-10-14 02:06:18 +0900

    1014_0206

---
+++ calender.js
@@ -0,0 +1,14 @@
+function SetCalender(startDate, startDay){
+    var datetime = new Date();
+
+    //alert(datetime)
+
+    // 캘린더에 날짜 맞춰넣기
+    for(var week = 1 ; week <= 4 ; week++){
+        for(var day = 0 ; day < 7 ; day++){
+
+        }
+    }
+}
+
+SetCalender();
